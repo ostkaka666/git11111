04fd4c992a70f6549e27674531fa41e5430e7d72
menglong hu <ostkaka66@gmail.com>
2019-10-08 13:23:18 +0800

    init

---
+++ ff/ff/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ff
+//
+//  Created by 马家兴 on 2019/10/7.
+//  Copyright © 2019 马家兴. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ ff/ffTests/ffTests.m
@@ -0,0 +1,37 @@
+//
+//  ffTests.m
+//  ffTests
+//
+//  Created by 马家兴 on 2019/10/7.
+//  Copyright © 2019 马家兴. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ffTests : XCTestCase
+
+@end
+
+@implementation ffTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
